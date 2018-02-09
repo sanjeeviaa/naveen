bbbe4d4c0ef4ec6b481b271e09a701cae849fb68
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-02-09 14:30:15 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+#include <string.h>
+ 
+void main()
+{
+   char arr[100];
+ 
+   printf("Enter a string to reverse\n");
+   gets(arr);
+ 
+   strrev(arr);
+ 
+   printf("Reverse of the string is \n%s\n", arr);
+ 
+   
+}
