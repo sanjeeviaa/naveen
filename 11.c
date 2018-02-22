20a2133e988225de0971798b0d1f069ce737b894
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-02-22 16:27:57 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    char str[7][20];
+    printf("Enter the day");
+    scanf("%s",str);
+    if((str[0][0]=='s'))
+    printf("Yes it is hol");
+    else
+    printf("No");
+    
+}
