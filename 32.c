360ae4fed670933ae260b5eaa85d130cc01b7f85
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-03-02 16:05:01 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int n,k,i,a[10];
+scanf("%d %d\n",&n,&k);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+if(a[i]==k)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
+
