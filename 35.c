bba0109cbfcf7d1041d1a35dc7591681f5454686
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-03-02 16:47:24 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n,i,j;
+scanf("%d",&n);
+char a[100][10];
+
+for ( i = 0; i < n; i++) {
+    scanf("%s", &a[i][12]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]==a[j])
+{
+printf("resulted strintg %s",a[i]);
+}
+}
