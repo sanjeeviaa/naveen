0579082ac3c5e591607a579972c3f7f3bf71aa8f
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-03-08 15:57:51 +0530

    Create 38.c

---
+++ 38.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int i,n;
+	printf("Enter the Number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			if(i%2==0)
+			{
+				printf("%d ",i);
+			}
+		}
+	}
+}
