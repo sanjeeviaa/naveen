88767670206f4f081ca888ab1fe94c1d010d1658
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-03-13 16:57:09 +0530

    Create 55a.c

---
+++ 55a.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,flag=0;
+    char a[10],b[10];
+    printf("\n Enter two strings:");
+    scanf("%s %s",a,b);
+    for(i=0,j=0;a[i]!='\0',b[j]!='\0';i++,j++)
+    {
+        if(a[i]!=b[j])
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    printf("\n YES");
+    else
+    printf("\n NO");
+    return 0;
+}
